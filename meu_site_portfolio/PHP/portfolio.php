283a5be5d5f8a08fc89a7613a71b307b5a5351bc
Rodrigo Diniz da Silva <rodrigo_diniz_da_silva@yahoo.com>
2025-02-22 17:01:13 -0300

    Substituí algumas imagens para obter um melhor realce pois a cor da janela do navegador estava um pouco fraca nas imagens anteriores.

--- meu_site_portfolio/PHP/portfolio.php
+++ meu_site_portfolio/PHP/portfolio.php
@@ -31,7 +31,7 @@ function carregar_pagina(){
   $array_create_test_tabs_firefox = array();
   $array_create_test_tabs_firefox['nome'] = 'Create Test Tabs FireFox';
   $array_create_test_tabs_firefox['identificador'] = 'create_test_tabs_firefox';
-  $array_create_test_tabs_firefox['imagem_destaque'] = 'https://i.imgur.com/xLQ0rAS.png';
+  $array_create_test_tabs_firefox['imagem_destaque'] = 'https://i.imgur.com/7wX83HX.png';
   $array_create_test_tabs_firefox['descricao'] = 'Este projeto é um addon para navegadores Mozilla 
 (FireFox, WaterFox e etc) que permite criar abas com formulários preenchidos, facilitando os testes.
 Este addon permite que você registre páginas com formulários do seu projeto, combinações de entradas 
@@ -55,7 +55,7 @@ Acesse a página do Create Test Tabs FireFox no Addons Mozilla:
 <a href="https://addons.mozilla.org/firefox/addon/create-test-tabs/" target="blank">
 https://addons.mozilla.org/firefox/addon/create-test-tabs/</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/xLQ0rAS.png';
+  $array_imagens[] = 'https://i.imgur.com/7wX83HX.png';
   $array_imagens[] = 'https://i.imgur.com/n1bAocC.png';
   $array_imagens[] = 'https://i.imgur.com/aJrzQNu.png';
   $array_imagens[] = 'https://i.imgur.com/VjwiHnh.png';
@@ -66,8 +66,8 @@ https://addons.mozilla.org/firefox/addon/create-test-tabs/</a>';
   $array_imagens[] = 'https://i.imgur.com/2ekxtKB.png';
   $array_imagens[] = 'https://i.imgur.com/iSLEQ0b.png';
   $array_imagens[] = 'https://i.imgur.com/nVexrkF.png';
-  $array_imagens[] = 'https://i.imgur.com/gPN8H8b.png';
-  $array_imagens[] = 'https://i.imgur.com/5CVfrjY.png';
+  $array_imagens[] = 'https://i.imgur.com/eWzjvkq.png';
+  $array_imagens[] = 'https://i.imgur.com/PdoWYow.png';
   $array_create_test_tabs_firefox['imagens'] = $array_imagens;
   $array_videos = array();
   $array_create_test_tabs_firefox['videos'] = $array_videos;
@@ -77,7 +77,7 @@ https://addons.mozilla.org/firefox/addon/create-test-tabs/</a>';
   $array_crud_laravel_bootstrap = array();
   $array_crud_laravel_bootstrap['nome'] = 'CRUD Laravel Bootstrap';
   $array_crud_laravel_bootstrap['identificador'] = 'crud_laravel_bootstrap';
-  $array_crud_laravel_bootstrap['imagem_destaque'] = 'https://i.imgur.com/BYzoAss.png';
+  $array_crud_laravel_bootstrap['imagem_destaque'] = 'https://i.imgur.com/YMqL3cc.png';
   $array_crud_laravel_bootstrap['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, 
 HTML, CSS e Bootstrap: Formulários de inclusão, formulários de edição, listas paginadas, filtros, 
 ordenações, visualização detalhada, exclusão, associação única (radio button), mensagens de aviso, 
@@ -92,7 +92,7 @@ Acesse a página do CRUD Laravel Bootstrap no GitHub:
 <a href="https://github.com/rodrigo2016rj/crud_laravel_bootstrap" target="blank">
 https://github.com/rodrigo2016rj/crud_laravel_bootstrap</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/BYzoAss.png';
+  $array_imagens[] = 'https://i.imgur.com/YMqL3cc.png';
   $array_imagens[] = 'https://i.imgur.com/i6kuSlr.png';
   $array_imagens[] = 'https://i.imgur.com/UoGbJzp.png';
   $array_imagens[] = 'https://i.imgur.com/8y090GL.png';
@@ -121,7 +121,7 @@ https://github.com/rodrigo2016rj/crud_laravel_bootstrap</a>';
   $array_crud_laravel_javascript = array();
   $array_crud_laravel_javascript['nome'] = 'CRUD Laravel Javascript';
   $array_crud_laravel_javascript['identificador'] = 'crud_laravel_javascript';
-  $array_crud_laravel_javascript['imagem_destaque'] = 'https://i.imgur.com/KoIuwEs.png';
+  $array_crud_laravel_javascript['imagem_destaque'] = 'https://i.imgur.com/k0RA632.png';
   $array_crud_laravel_javascript['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, 
 HTML e CSS: Formulários de inclusão, formulários de edição, listas paginadas, filtros, ordenações, 
 visualização detalhada, exclusão, associação única (radio button), mensagens de aviso, popups com 
@@ -136,7 +136,7 @@ Acesse a página do CRUD Laravel Javascript no GitHub:
 <a href="https://github.com/rodrigo2016rj/crud_laravel_javascript" target="blank">
 https://github.com/rodrigo2016rj/crud_laravel_javascript</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/KoIuwEs.png';
+  $array_imagens[] = 'https://i.imgur.com/k0RA632.png';
   $array_imagens[] = 'https://i.imgur.com/6Wxkv7s.png';
   $array_imagens[] = 'https://i.imgur.com/FDxss7B.png';
   $array_imagens[] = 'https://i.imgur.com/jsPkFej.png';
@@ -169,7 +169,7 @@ https://github.com/rodrigo2016rj/crud_laravel_javascript</a>';
   $array_crud_laravel_jquery = array();
   $array_crud_laravel_jquery['nome'] = 'CRUD Laravel jQuery';
   $array_crud_laravel_jquery['identificador'] = 'crud_laravel_jquery';
-  $array_crud_laravel_jquery['imagem_destaque'] = 'https://i.imgur.com/3ciVipD.png';
+  $array_crud_laravel_jquery['imagem_destaque'] = 'https://i.imgur.com/wPZOdHj.png';
   $array_crud_laravel_jquery['descricao'] = 'Implementei com PHP, Laravel, MySQL, jQuery, HTML e 
 CSS: Formulários de inclusão, formulários de edição, listas paginadas, filtros, ordenações, 
 visualização detalhada, exclusão, associação única (radio button), mensagens de aviso, popups com 
@@ -184,7 +184,7 @@ Acesse a página do CRUD Laravel jQuery no GitHub:
 <a href="https://github.com/rodrigo2016rj/crud_laravel_jquery" target="blank">
 https://github.com/rodrigo2016rj/crud_laravel_jquery</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/3ciVipD.png';
+  $array_imagens[] = 'https://i.imgur.com/wPZOdHj.png';
   $array_imagens[] = 'https://i.imgur.com/00Wkg1I.png';
   $array_imagens[] = 'https://i.imgur.com/bNcJbDq.png';
   $array_imagens[] = 'https://i.imgur.com/Y0esu9k.png';
@@ -216,7 +216,7 @@ https://github.com/rodrigo2016rj/crud_laravel_jquery</a>';
   $array_crud_laravel_react = array();
   $array_crud_laravel_react['nome'] = 'CRUD Laravel React';
   $array_crud_laravel_react['identificador'] = 'crud_laravel_react';
-  $array_crud_laravel_react['imagem_destaque'] = 'https://i.imgur.com/L1rJerl.png';
+  $array_crud_laravel_react['imagem_destaque'] = 'https://i.imgur.com/O7HINTM.png';
   $array_crud_laravel_react['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, React, 
 HTML e CSS: Formulários de inclusão, formulários de edição, listas paginadas, filtros, ordenações, 
 visualização detalhada, exclusão, associação única (radio button), mensagens de aviso, popups com 
@@ -231,7 +231,7 @@ Acesse a página do CRUD Laravel React no GitHub:
 <a href="https://github.com/rodrigo2016rj/crud_laravel_react" target="blank">
 https://github.com/rodrigo2016rj/crud_laravel_react</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/L1rJerl.png';
+  $array_imagens[] = 'https://i.imgur.com/O7HINTM.png';
   $array_imagens[] = 'https://i.imgur.com/CwAwIHY.png';
   $array_imagens[] = 'https://i.imgur.com/kOpGHVh.png';
   $array_imagens[] = 'https://i.imgur.com/dAvsAsb.png';
@@ -265,7 +265,7 @@ https://github.com/rodrigo2016rj/crud_laravel_react</a>';
   $array_crud_laravel_vue_inertia = array();
   $array_crud_laravel_vue_inertia['nome'] = 'CRUD Laravel Vue Inertia';
   $array_crud_laravel_vue_inertia['identificador'] = 'crud_laravel_vue_inertia';
-  $array_crud_laravel_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/AO1rIuA.png';
+  $array_crud_laravel_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/1intl74.png';
   $array_crud_laravel_vue_inertia['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, 
 Vue, Inertia, HTML e CSS: Formulários de inclusão, formulários de edição, listas paginadas, filtros, 
 ordenações, visualização detalhada, exclusão, associação única (radio button), mensagens de aviso, 
@@ -280,7 +280,7 @@ Acesse a página do CRUD Laravel Vue Inertia no GitHub:
 <a href="https://github.com/rodrigo2016rj/crud_laravel_vue_inertia" target="blank">
 https://github.com/rodrigo2016rj/crud_laravel_vue_inertia</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/AO1rIuA.png';
+  $array_imagens[] = 'https://i.imgur.com/1intl74.png';
   $array_imagens[] = 'https://i.imgur.com/g5E6v3w.png';
   $array_imagens[] = 'https://i.imgur.com/rRY8YnZ.png';
   $array_imagens[] = 'https://i.imgur.com/nWQryVE.png';
@@ -317,7 +317,7 @@ https://github.com/rodrigo2016rj/crud_laravel_vue_inertia</a>';
   $array_demonstracao_de_slides_com_code_igniter = array();
   $array_demonstracao_de_slides_com_code_igniter['nome'] = 'Demonstração de Slides com Code Igniter';
   $array_demonstracao_de_slides_com_code_igniter['identificador'] = 'demonstracao_de_slides_com_code_igniter';
-  $array_demonstracao_de_slides_com_code_igniter['imagem_destaque'] = 'https://i.imgur.com/xHuv5Tp.png';
+  $array_demonstracao_de_slides_com_code_igniter['imagem_destaque'] = 'https://i.imgur.com/OfcqkcL.png';
   $array_demonstracao_de_slides_com_code_igniter['descricao'] = 'Implementei com PHP, CodeIgniter, 
 Smarty, MySQL, HTML e CSS: Slide que fiz com Javascript, slide que fiz com jQuery e slide que fiz 
 com React. Cada slide possui: Movimento automático, botão para parar movimento automático, botão 
@@ -335,7 +335,7 @@ Acesse a página do Demonstração de Slides com Code Igniter no GitHub:
 <a href="https://github.com/rodrigo2016rj/demonstracao_de_slides_com_code_igniter" target="blank">
 https://github.com/rodrigo2016rj/demonstracao_de_slides_com_code_igniter</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/xHuv5Tp.png';
+  $array_imagens[] = 'https://i.imgur.com/OfcqkcL.png';
   $array_imagens[] = 'https://i.imgur.com/DDSRAIN.png';
   $array_imagens[] = 'https://i.imgur.com/A8AYaGx.png';
   $array_imagens[] = 'https://i.imgur.com/29XIGbe.png';
@@ -360,7 +360,7 @@ https://github.com/rodrigo2016rj/demonstracao_de_slides_com_code_igniter</a>';
   $array_desafio_da_construp = array();
   $array_desafio_da_construp['nome'] = 'Desafio da Construp';
   $array_desafio_da_construp['identificador'] = 'desafio_da_construp';
-  $array_desafio_da_construp['imagem_destaque'] = 'https://i.imgur.com/vnCVfZg.png';
+  $array_desafio_da_construp['imagem_destaque'] = 'https://i.imgur.com/M9vc4Zu.png';
   $array_desafio_da_construp['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, HTML e 
 CSS, uma tabela (tag table) paginada que mostra materiais de construção com as seguintes 
 informações: momento do cadastrado, nome, descrição, marca, valor e opções. Implementei para que a 
@@ -381,7 +381,7 @@ Acesse a página do Desafio da Construp no GitLab:
 <a href="https://gitlab.com/rodrigo2016rj/desafio_da_construp" target="blank">
 https://gitlab.com/rodrigo2016rj/desafio_da_construp</a>';
   $array_imagens = array();
-  $array_imagens[] = 'https://i.imgur.com/vnCVfZg.png';
+  $array_imagens[] = 'https://i.imgur.com/M9vc4Zu.png';
   $array_imagens[] = 'https://i.imgur.com/qn4t8yD.png';
   $array_imagens[] = 'https://i.imgur.com/jeHtezG.png';
   $array_imagens[] = 'https://i.imgur.com/VuSXaf2.png';
@@ -420,7 +420,7 @@ https://gitlab.com/rodrigo2016rj/desafio_da_construp</a>';
   $array_login_codeigniter_jquery = array();
   $array_login_codeigniter_jquery['nome'] = 'Login CodeIgniter jQuery';
   $array_login_codeigniter_jquery['identificador'] = 'login_codeigniter_jquery';
-  $array_login_codeigniter_jquery['imagem_destaque'] = 'https://i.imgur.com/oMtpYWu.png';
+  $array_login_codeigniter_jquery['imagem_destaque'] = 'https://i.imgur.com/h7MHVZx.png';
   $array_login_codeigniter_jquery['descricao'] = 'Implementei com PHP, CodeIgniter, Smarty, MySQL, 
 jQuery, HTML e CSS: Formulário de cadastro com confirmação via e-mail, formulário de login, tabela 
 de usuários com navegação contínua (com link para carregar mais registros), formulário de busca com 
@@ -449,7 +449,7 @@ https://github.com/rodrigo2016rj/login_codeigniter_jquery</a>';
   $array_login_laravel_pdo_vue_inertia = array();
   $array_login_laravel_pdo_vue_inertia['nome'] = 'Login Laravel PDO Vue Inertia';
   $array_login_laravel_pdo_vue_inertia['identificador'] = 'login_laravel_pdo_vue_inertia';
-  $array_login_laravel_pdo_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/JiNAi9L.png';
+  $array_login_laravel_pdo_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/7RqCBqP.png';
   $array_login_laravel_pdo_vue_inertia['descricao'] = 'Implementei com PHP, Laravel, PDO, MySQL, 
 Javascript, Vue, Inertia, HTML e CSS: Formulário de cadastro, formulário de login, tabela de 
 usuários com navegação contínua (com link para carregar mais registros), formulário de busca com 
@@ -479,7 +479,7 @@ https://github.com/rodrigo2016rj/login_laravel_pdo_vue_inertia</a>';
   $array_servicos_laravel_vue_inertia = array();
   $array_servicos_laravel_vue_inertia['nome'] = 'Serviços Laravel Vue Inertia';
   $array_servicos_laravel_vue_inertia['identificador'] = 'servicos_laravel_vue_inertia';
-  $array_servicos_laravel_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/fHHVhek.png';
+  $array_servicos_laravel_vue_inertia['imagem_destaque'] = 'https://i.imgur.com/tRloTpA.png';
   $array_servicos_laravel_vue_inertia['descricao'] = 'Implementei com PHP, Laravel, MySQL, 
 Javascript, Vue, Inertia, HTML e CSS: Um sistema que consome serviços web de outros sistemas. 
 Implementei neste sistema: Um menu lateral com grupos de links; A Página Inicial com texto que 
@@ -510,7 +510,7 @@ https://github.com/rodrigo2016rj/servicos_laravel_vue_inertia</a>';
   $array_teste_da_clinica_experts = array();
   $array_teste_da_clinica_experts['nome'] = 'Teste da Clínica Experts';
   $array_teste_da_clinica_experts['identificador'] = 'teste_da_clinica_experts';
-  $array_teste_da_clinica_experts['imagem_destaque'] = 'https://i.imgur.com/e9Ueqfd.png';
+  $array_teste_da_clinica_experts['imagem_destaque'] = 'https://i.imgur.com/OtLwksh.png';
   $array_teste_da_clinica_experts['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, 
 Vue, Inertia, HTML e CSS um sistema encurtador de links que: Exibe um formulário com os campos url e 
 identificador, valida a url e o identificador (quando enviado pelo usuário), gera um identificador 
@@ -541,7 +541,7 @@ https://gitlab.com/rodrigo2016rj/teste_da_clinica_experts</a>';
   $array_teste_da_proconsult = array();
   $array_teste_da_proconsult['nome'] = 'Teste da Proconsult';
   $array_teste_da_proconsult['identificador'] = 'teste_da_proconsult';
-  $array_teste_da_proconsult['imagem_destaque'] = 'https://i.imgur.com/YKrkOwg.png';
+  $array_teste_da_proconsult['imagem_destaque'] = 'https://i.imgur.com/CLrQbHN.png';
   $array_teste_da_proconsult['descricao'] = 'Implementei com PHP, Laravel, MySQL, Javascript, Vue, 
 Inertia, HTML e CSS um sistema que possui: Página inicial com informações sobre o sistema; Página de 
 cadastro com validações, mensagens e máscara para o campo CPF; Formulário de login exibido em um 
